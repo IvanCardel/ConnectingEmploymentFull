861415969e3568b3d51d5fce1d9ca56e0b9c4475
IvanCardel <jics080493@hotmail.com>
2021-06-25 23:17:59 -0500

    Ahí la llevo

---
+++ conec.php
@@ -0,0 +1,4 @@
+<?php 
+    $cn=mysqli_connect("localhost","root","","connecting");
+    // $cn=mysqli_connect("localhost","id15935492_conectandoempleo","ConectandoEmpleos12345.","id15935492_connecting");
+?>
